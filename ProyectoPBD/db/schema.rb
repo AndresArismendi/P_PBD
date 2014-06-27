0e67e536b575b3f39aae9da9a3e9763d101b56d8
AndresArismendi <andres.arismendi@usach.cl>
2014-06-26 21:16:07 -0400

    modificaciones 26-6-2014
    vistas del administrador

--- ProyectoPBD/db/schema.rb
+++ ProyectoPBD/db/schema.rb
@@ -262,8 +262,8 @@ ActiveRecord::Schema.define(version: 20140625044208) do
   add_index "travels", ["run_driv"], name: "relationship_13_fk", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",        null: false
+    t.string   "encrypted_password",     default: "",        null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -272,7 +272,7 @@ ActiveRecord::Schema.define(version: 20140625044208) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "tipo"
+    t.string   "tipo",                   default: "cli_nat"
     t.string   "rut"
     t.string   "nombre"
     t.string   "apellidos"
